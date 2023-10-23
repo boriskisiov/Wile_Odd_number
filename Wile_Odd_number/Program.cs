3b0573e72c4ca9e85b84c8ee70134f8ceec04d7c
boriskisiov <boriskisiov@gmail.com>
2023-10-23 16:08:21 +0300

    Add project files.

---
+++ Wile_Odd_number/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Wile_Odd_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            while (num % 2 == 0) {
+                num = int.Parse(Console.ReadLine());
+            
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
